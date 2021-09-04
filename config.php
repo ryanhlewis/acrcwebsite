c0f01593182e6c5b118b1844018e73ecd2775f8b
ryanhlewis <lewisrya001@mysbisd.org>
2021-09-04 18:16:37 -0500

    Integration

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); /* Session */
-$con=pg_connect("host=ec2-34-194-14-176.compute-1.amazonaws.com user=jfaukfvoobzpif password=ee9be38f29077e0590f5f880370544deb52e54586cc4f4f0b0a6762b2ee1e929 dbname=d1ouseq16tgvj6 port=5432"); /*Database Connection*/
-// Check connection
-
-?>
